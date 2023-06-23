b9301b58a1930648e6f701b8661e6fd82a3d9b19
Jesus Salinas <jjesus.salinas@gmail.com>
2023-06-23 21:02:52 +0000

    Finish custom filters

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -6,9 +6,12 @@ user_model = get_user_model()
 register = template.Library()
 
 @register.filter
-def author_details(author):
+def author_details(author, current_user=None):
   if not isinstance(author, user_model):
     return ""
+
+  if author == current_user:
+    return format_html("<strong>me</strong>")
   
   if author.first_name and author.last_name:
     name = f"{author.first_name} {author.last_name}"
