cddadb82f0c7488d60dd810fc33ae98560d4261e
Jose Vela <jjesus.salinas@gmail.com>
2023-07-19 23:25:30 +0000

    Add a Custom User Model Part 1

--- blango/settings.py
+++ blango/settings.py
@@ -40,6 +40,8 @@ class Dev(Configuration):
 
     INTERNAL_IPS = ["192.168.10.93"]
 
+    AUTH_USER_MODEL = "blango_auth.User"
+
     #Logging
     LOGGING = {
         "version": 1,
@@ -97,6 +99,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'blango_auth',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
