bb6c1e446b80ebf587a8305ed376cd033f5a3b1c
Jose Vela <jjesus.salinas@gmail.com>
2023-07-25 20:49:56 +0000

    Two-Step Activation Part 1 and 2

--- blango/urls.py
+++ blango/urls.py
@@ -19,6 +19,8 @@ from blog import views
 import debug_toolbar
 from django.conf import settings
 import blango_auth.views
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +29,8 @@ urlpatterns = [
     path("ip/", views.get_ip),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
+    path("accounts/register/", RegistrationView.as_view(form_class=BlangoRegistrationForm), name="django_registration_register"),
+    path("accounts/", include("django_registration.backends.activation.urls")),
 ]
 
 if settings.DEBUG:
